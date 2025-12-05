be1f40a4282e6fe4fb08410dfd51ee0bff25f877
George <georgemize100@gmail.com>
2025-12-05 16:33:29 -0600

    add player logic

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+
+@onready var aww := $music/aww
+@onready var noo := $music/noooo
+
+
+
+func music_config():
+	aww.stream_paused = true
+	noo.playing = true
+	
+func _ready() -> void:
+	ChudPlayer.fall.emit(music_config())
