ade59c0cf043d388499f8fc777c80c2224577a02
George <georgemize100@gmail.com>
2025-12-07 13:26:32 -0600

    work in progress animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,12 +5,12 @@ extends CharacterBody2D
 const SPEED := 300.0
 const JUMP_VELOCITY := -400.0
 var falling := false
-
+@onready var player_animation := $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
 
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("Fall"):
 		falling = true
 	if not is_on_floor() and falling:
 		velocity += get_gravity() * delta
