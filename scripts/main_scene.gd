32308aff41c6778cb76cbe6f9906edd84bb37a4b
George <georgemize100@gmail.com>
2025-12-15 21:10:01 -0600

    reset back to bare bones player script, added laser scene and new sprite

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,21 +1,19 @@
 extends Node2D
 
 
-
+@onready var laser_bullet := preload("res://scenes/laser_bullet.tscn")
 @onready var enemy := preload("res://scenes/enemy.tscn")
-@onready var enemy_spawn_timer := $Timer
+@onready var player := $Player
 
 
 
 func _on_timer_timeout() -> void:
 
-
-	enemy_instance(Vector2(randi_range(-100,100),32))
+	enemy_instance(Vector2(randi_range(-100,100),35))
 
 
 
 func enemy_instance(pos):
-
 
 	var instance = enemy.instantiate()
 	instance.position = pos
@@ -24,8 +22,17 @@ func enemy_instance(pos):
 
 
 
-func _process(_delta: float) -> void:
-	
-	
-	
-	pass
+func bullet_shoot(pos):
+
+	var bullet = laser_bullet.instantiate()
+	bullet.position = pos
+	self.add_child(bullet)
+
+
+
+func _physics_process(_delta: float) -> void:
+
+	if Input.is_action_just_pressed("Shoot"):
+		bullet_shoot(player.position)
+		
+
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
 
+@export var speed := 3000.0
 
-@export var player_speed := 3000.0
+
+
+func _ready() -> void:
+
+	pass
 
 
 
 func _physics_process(delta: float) -> void:
 
-
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
-		velocity.x = direction * player_speed * delta
+		velocity.x = direction * speed * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, player_speed)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
